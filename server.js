2a3478712af2a7790acf67f36d37c0267f8856bb
Vishnu Sai Bhosekar <vishnusaibhosekar@gmail.com>
2023-04-21 19:28:58 -0400

    Revert "adds logic to send gifs"
    This reverts commit 225b4b7d13c79281da4cac9a9426b6aa713652bc.

--- server.js
+++ server.js
@@ -1,35 +1,46 @@
+///////////////////////////////////////////////
+///////////// IMPORTS + VARIABLES /////////////
+///////////////////////////////////////////////
+
 const http = require('http'); 
 const CONSTANTS = require('./utils/constants.js');
 const fs = require('fs');
 const path = require('path');
 const WebSocket = require('ws');
-const Tenor = require('tenorjs').client({
-  "Key": "AIzaSyAW10NlH4lJfYGSj8E-CwhGV6aKyu81rfE",
-  "Filter": "high",
-  "Locale": "en_US",
-});
 
+// You may choose to use the constants defined in the file below
 const { PORT, CLIENT, SERVER } = CONSTANTS;
 
+///////////////////////////////////////////////
+///////////// HTTP SERVER LOGIC ///////////////
+///////////////////////////////////////////////
+
+// Create the HTTP server
 const server = http.createServer((req, res) => {
-  
+  // get the file path from req.url, or '/public/index.html' if req.url is '/'
   const filePath = ( req.url === '/' ) ? '/public/index.html' : req.url;
-  
+
+  // determine the contentType by the file extension
   const extname = path.extname(filePath);
   let contentType = 'text/html';
   if (extname === '.js') contentType = 'text/javascript';
   else if (extname === '.css') contentType = 'text/css';
 
+  // pipe the proper file to the res object
   res.writeHead(200, { 'Content-Type': contentType });
   fs.createReadStream(`${__dirname}/${filePath}`, 'utf8').pipe(res);
 });
+
+///////////////////////////////////////////////
+////////////////// WS LOGIC ///////////////////
+///////////////////////////////////////////////
 
 const wsServer = new WebSocket.Server({ server });
 
 wsServer.on('connection', (socket) => {
   console.log('Client connected');
 
-  socket.on('message', async (data) => { // add async to support await in this function
+  socket.on('message', (data) => {
     const { type, payload } = JSON.parse(data);
     switch(type) {
       case CLIENT.MESSAGE.NEW_USER:
@@ -42,27 +53,17 @@ wsServer.on('connection', (socket) => {
         broadcast(JSON.stringify(dataWithTime));
         break;
       case CLIENT.MESSAGE.NEW_MESSAGE:
-        if (payload.message.toLowerCase().startsWith('/gif ')) {
-          const query = payload.message.slice(5);
-          const url = `https://api.tenor.com/v1/search?q=${query}&key=${TENOR_API_KEY}&contentfilter=low`;
-          try {
-            const response = await fetch(url);
-            const { results } = await response.json();
-            const gifUrl = results[Math.floor(Math.random() * results.length)].media[0].gif.url;
-            payload.message = gifUrl;
-            broadcast(JSON.stringify({ type: CLIENT.MESSAGE.NEW_MESSAGE, payload }), socket);
-          } catch (err) {
-            console.error(err);
-          }
-        } else {
-          broadcast(data, socket);
-        }
+        broadcast(data, socket);
         break;
       default:
         break;
     }
   });
 });
+
+///////////////////////////////////////////////
+////////////// HELPER FUNCTIONS ///////////////
+///////////////////////////////////////////////
 
 function broadcast(data, socketToOmit) {
   wsServer.clients.forEach(connectedSocket => {
@@ -72,6 +73,8 @@ function broadcast(data, socketToOmit) {
   });
 }
 
+// Start the server listening on localhost:8080
 server.listen(PORT, () => {
   console.log(`Listening on: http://localhost:${server.address().port}`);
 });
+
